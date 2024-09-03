900a18bb518ccf1f98fd157529b1a09a5bd4d3bc
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-09-03 12:22:36 +0300

    Hash routing adjustement

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"; // Use HashRouter instead of BrowserRouter
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router basename="#">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
