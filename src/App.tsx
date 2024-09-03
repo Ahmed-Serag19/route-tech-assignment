cbf76a3938537f653668dd3c35f5cd6d7415bf93
Ahmed Mohamed <80613530+Ahmed-Serag19@users.noreply.github.com>
2024-09-03 11:59:52 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,18 @@ import CustomerDetail from './routes/CustomerDetail';
 import { Customer, Transaction } from './services/types';
 
 const App: React.FC = () => {
-  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [customers, setCustomers] = useState<Customer[]>([
+  { id: 1, name: "Ahmed Ali" },
+  { id: 2, name: "Aya Elsayed" },
+  { id: 3, name: "Mina Adel" },
+  { id: 4, name: "Sarah Reda" },
+  { id: 5, name: "Mohamed Sayed" },
+  { id: 6, name: "Hassan Khalil" },
+  { id: 7, name: "Laila Ahmed" },
+  { id: 8, name: "Omar Zaki" },
+  { id: 9, name: "Nora Sami" },
+  { id: 10, name: "Tarek Youssef" }
+]);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
